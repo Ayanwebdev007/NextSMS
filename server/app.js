e524cc9b9705c2baae065785046619f0d6891f2e
GS3 SOLUTION <gs3websolutions@gmail.com>
2026-01-14 20:49:28 +0530

    fix(server): use middleware for SPA catch-all to prevent PathError crash

--- server/app.js
+++ server/app.js
@@ -147,7 +147,10 @@ app.use(express.static(clientBuildPath));
 // The "catch-all" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 // We use a regex /* to be safe with modern Express/path-to-regexp
-app.get('*', (req, res) => {
+app.use((req, res, next) => {
+  if (req.method !== 'GET' || req.url.startsWith('/api')) {
+    return next();
+  }
   const indexPath = path.join(clientBuildPath, 'index.html');
   res.sendFile(indexPath, (err) => {
     if (err) {
